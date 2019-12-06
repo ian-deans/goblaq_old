363720895cb06e29c0a5dd7a1b38357c5cdee59b
Ian Deans <ideans715@gmail.com>
2019-12-05 20:19:11 -0800

    initial commit

---
+++ pages/forums/list.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const ForumList: React.FC = () => {
+
+  return (
+    <div>
+      <header>
+        <h1>Forum List</h1>
+      </header>
+      <section>
+        <ul>
+          <li>Forum Link</li>
+          <li>Forum Link</li>
+        </ul>
+      </section>
+    </div>
+  );
+};
+
+export default ForumList;
