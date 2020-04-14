9420cb2f27ab74c520d35f9b1d15db09c5bf56b9
ypsource <63256939+ypsource@users.noreply.github.com>
2020-04-14 01:07:45 -0500

    Update contact.tsx

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,6 +1,6 @@
 /* Home */
 import React from "react";
-import Script from 'react-load-script';
+import Script from "react-load-script";
 import { Page } from "../src/components/common/Page";
 
 const Contact: React.SFC = (props: any) => {
