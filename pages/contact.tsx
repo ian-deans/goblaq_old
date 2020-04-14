9171e8ec78ec0bcfaf905934570ad8951bf20b9c
ypsource <63256939+ypsource@users.noreply.github.com>
2020-04-14 01:05:05 -0500

    Update contact.tsx

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,19 +1,14 @@
 /* Home */
 import React from "react";
+import Script from 'react-load-script';
 import { Page } from "../src/components/common/Page";
 
 const Contact: React.SFC = (props: any) => {
     return (
         <Page>
             <h2>Contact</h2>
-            "<script>
-	            window.fwSettings={
-	            'widget_id':62000000571
-	            };
-	        !function(){if("function"!=typeof window.FreshworksWidget){var n=function(){n.q.push(arguments)};n.q=[],window.FreshworksWidget=n}}()
-            </script>
-            <script type='text/javascript' src='https://widget.freshworks.com/widgets/62000000571.js' async defer></script>
-        </Page>"
+           
+        </Page>
     );
 };
 
