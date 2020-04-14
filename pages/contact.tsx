a1df0736a0ce013331da430bf6761c5b5d9d9ef3
ypsource <63256939+ypsource@users.noreply.github.com>
2020-04-14 00:41:47 -0500

    Update contact.tsx

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -6,14 +6,14 @@ const Contact: React.SFC = (props: any) => {
     return (
         <Page>
             <h2>Contact</h2>
-            <script>
+            "<script>
 	            window.fwSettings={
 	            'widget_id':62000000571
 	            };
 	        !function(){if("function"!=typeof window.FreshworksWidget){var n=function(){n.q.push(arguments)};n.q=[],window.FreshworksWidget=n}}()
             </script>
             <script type='text/javascript' src='https://widget.freshworks.com/widgets/62000000571.js' async defer></script>
-        </Page>
+        </Page>"
     );
 };
 
