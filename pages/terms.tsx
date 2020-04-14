8083e63e687f3614acddc7861a01845332a71840
ypsource <63256939+ypsource@users.noreply.github.com>
2020-04-14 00:32:10 -0500

    add content

--- pages/terms.tsx
+++ pages/terms.tsx
@@ -6,6 +6,14 @@ const Terms: React.SFC = (props: any) => {
     return (
         <Page>
             <h2>Terms and Conditions</h2>
+            <Typography variant="body1">
+            Last Updated on November 27, 2012. These terms and conditions are effective immediately for those registering accounts 
+            after that date and will become effective December 27, 2012 for those with pre-existing accounts.These terms and conditions 
+            (the Terms) govern your access to and use of GoBlaq’s websites and mobile applications that link to or reference 
+            these Terms (Site). By accessing or using the Site, you are agreeing to these Terms and concluding a legally binding 
+            contract with GoBlaq Inc., a Delaware corporation headquartered in San Francisco, California (GoBlaq). 
+            Do not access or use the Site if you are unwilling or unable to be bound by the Terms.
+            </Typography>
         </Page>
     );
 };
