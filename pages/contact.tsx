6da5777f75fcaa8f595b186a9ffb6438c9c65000
ypsource <63256939+ypsource@users.noreply.github.com>
2020-04-14 00:38:10 -0500

    add freshdesk script

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -6,6 +6,13 @@ const Contact: React.SFC = (props: any) => {
     return (
         <Page>
             <h2>Contact</h2>
+            <script>
+	            window.fwSettings={
+	            'widget_id':62000000571
+	            };
+	        !function(){if("function"!=typeof window.FreshworksWidget){var n=function(){n.q.push(arguments)};n.q=[],window.FreshworksWidget=n}}()
+            </script>
+            <script type='text/javascript' src='https://widget.freshworks.com/widgets/62000000571.js' async defer></script>
         </Page>
     );
 };
