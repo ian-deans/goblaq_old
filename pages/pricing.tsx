c318d9a8a96350e1c4c2b6d936b7e948a089dd9d
ypsource <63256939+ypsource@users.noreply.github.com>
2020-04-15 10:21:27 -0500

    Update pricing.tsx

--- pages/pricing.tsx
+++ pages/pricing.tsx
@@ -108,24 +108,7 @@ const tiers = [
     buttonVariant: 'outlined',
   },
 ];
-const footers = [
-  {
-    title: 'Company',
-    description: ['Team', 'History', 'Contact us', 'Locations'],
-  },
-  {
-    title: 'Features',
-    description: ['Cool stuff', 'Random feature', 'Team feature', 'Developer stuff', 'Another one'],
-  },
-  {
-    title: 'Resources',
-    description: ['Resource', 'Resource name', 'Another resource', 'Final resource'],
-  },
-  {
-    title: 'Legal',
-    description: ['Privacy policy', 'Terms of use'],
-  },
-];
+
 
 export default function Pricing() {
   const classes = useStyles({});
