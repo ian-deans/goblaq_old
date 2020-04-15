d2adba74bd30ad5f860496d2447d81f4869326b1
nenaji <56316262+nenaji@users.noreply.github.com>
2020-04-15 09:11:17 -0500

    Update about.tsx

--- pages/about.tsx
+++ pages/about.tsx
@@ -8,18 +8,19 @@ const About: React.SFC = (props: any) => {
         <Page>
             <h2>About</h2>
             <Typography variant="body1">
-               Goblaq is an online engagement platform that empowers the  African Americans community to build, 
+ Goblaq is an online engagement platform that empowers the  African Americans community to build, 
                 launch and grow an online community.
             </Typography>
             
             <Typography variant="body1">
-             Our goal is to connect black-owned local businesses with millions of users looking for their products.
+ Our goal is to connect black-owned local businesses with millions of users looking for their products.
             </Typography>
             
-            <Typography variant="body1">
-              Goblaq customized solutions designed to optimize business impact by putting the  retailer’s brand first with a 
+                  <Typography variant="body1">
+ Goblaq customized solutions designed to optimize business impact by putting the  retailer’s brand first with a 
                 flexible pricing options.
             </Typography>
+
 
             <div>
                 <Typography variant="h5">
