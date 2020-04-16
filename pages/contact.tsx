9c9cea33a0553a9ae66ba779b407561af9861cbe
Ian <ideans715@gmail.com>
2020-04-16 12:15:11 -0700

    add freshdesk widget as iframe

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -3,9 +3,11 @@ import React from "react";
 import { Page } from "../src/components/common/Page";
 
 const Contact: React.SFC = (props: any) => {
+    const style = { border: "none" }
     return (
         <Page>
             <h2>Contact</h2>
+            <iframe style={style} width="100%" height="500px" src="https://goblaq.freshdesk.com/widgets/feedback_widget/new?"></iframe>
         </Page>
     );
 };
